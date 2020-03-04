bbc7c6007b26cf7aba41cab7f682b6f7f68dfc81
Indranil Mitra <48361721+indranilsldu@users.noreply.github.com>
2020-03-04 21:48:39 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n)
+{
+    if(n==1)
+        return 0;
+    else if(n==2)
+        return 1;
+    else
+        return fibo(n-1)+fibo(n-2);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of terms: ";
+    cin >> n ;
+    cout << "The nth fibonacci term is " << fibo(n);
+    return 0;
+}
